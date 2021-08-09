5c895464a04da2b1227205c324a92d715d04237e
Yash Goyal <yashaa643@gmail.com>
2021-08-09 17:08:02 +0530

    merge

--- src/api/unsplash.tsx
+++ src/api/unsplash.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID a30ME_iKqzinJrzlU92IL0hIfM_99bO9OYNSolr7C2o'
+        Authorization: 'Client-ID JzVN8jfTJrmoJW2abbZ8Bn-sWXGOXp2MXxqZ2Q2ZmH4'
       }
 })
