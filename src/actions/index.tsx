c4362574a15130dc452b688436b7250e1036cc43
Yash Goyal <yashaa643@gmail.com>
2021-08-12 12:02:20 +0530

    feat : added caching to feed posts and user details

--- src/actions/index.tsx
+++ src/actions/index.tsx
@@ -7,58 +7,70 @@ import {
   TfetchUser,
 } from '../types';
 
-export const fetchPosts = () => async (dispatch: Dispatch<TfetchPosts | Terror> ) => {
+function _setWithExpiry(key: string, value: any, ttl: number) {
+    const now = new Date()
+    const item = {
+        value: value,
+        expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+function _getWithExpiry(key: string) {
+    const itemStr = localStorage.getItem(key)
+    if (!itemStr) {
+        return null
+    }
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+    if (now.getTime() > item.expiry) {
+        localStorage.removeItem(key)
+        return null
+    }
+    return item.value
+}
+
+function _dispatchError(error:any,dispatch: Dispatch<TfetchPosts | TfetchUser | Terror>){
+    if (error.response) {        //request was made but server responded with status code different then 2xx
+        if (error.response.status === 401) {
+            dispatch({ type: 'FETCH_USER_ERROR', payload: { err: true, errMessage: "401" } })
+        }                     
+        else if (error.response.status === 404) {
+            dispatch({ type: 'FETCH_USER_ERROR', payload: { err: true, errMessage: "404" } })
+        }
+    } else if (error.request) {                 //request was made but no response received
+        dispatch({ type: 'FETCH_USER_ERROR', payload: { err: true, errMessage: error.message } })
+    } else {                                    //error in request config
+        console.log('Error', error.message);
+    }
+    console.log(error.config);
+}
+
+
+export const fetchPosts = () => async (dispatch: Dispatch<TfetchPosts| TfetchUser | Terror>) => {
     clearUser();
     clearUserPosts();
-    const response = await unsplash.get('/photos/random', {
-        params: {
-            count: 10
-        },
-    }).catch(function (error) {
-        if (error.response) {
-            if(error.response.status===401){
-                dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : "401"}})
-            }                      //request was made but server responded with status code different then 2xx
-            else if(error.response.status===404){
-                dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : "404"}})
-            }
-        } else if (error.request) {                 //request was made but no response received
-            dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : error.message}})
-        } else {                                    //error in request config
-            console.log('Error', error.message);
+    const cache = _getWithExpiry("feedPosts")
+    if(!cache){
+        const response = await unsplash.get('/photos/random', {params: {count: 10},}).catch((error) => _dispatchError(error,dispatch));
+        if (response) {
+            _setWithExpiry("feedPosts",response.data,300000); //cache for 5 min
+            dispatch({ type: 'FETCH_POSTS', payload: response.data });
         }
-        console.log(error.config);
-    });
-    if (response)
-        dispatch({ type: 'FETCH_POSTS', payload: response.data });
+    }else{ dispatch({ type: 'FETCH_POSTS', payload: cache });}
 }
 
 export const fetchUser = (username: string) => async (dispatch: Dispatch<TfetchUser | Terror>) => {
-
     const url = '/users/' + username;
-    const response = await unsplash.get(url, {
-        params: {
-            username: username,
-        },
-    }).catch(function (error) {
-        if (error.response) {
-            if(error.response.status===401){
-                dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : "401"}})
-            }                      //request was made but server responded with status code different then 2xx
-            else if(error.response.status===404){
-                dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : "404"}})
-            }
-        } else if (error.request) {                 //request was made but no response received
-            dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : error.message}})
-        } else {                                    //error in request config
-            console.log('Error', error.message);
+    const cache = _getWithExpiry("$" + username)
+    if(!cache){
+        const response = await unsplash.get(url, {params: {username},}).catch((error) => _dispatchError(error,dispatch));
+        if (response) {
+            _setWithExpiry("$"+username,response.data,300000); //cache for 5 min
+            dispatch({ type: 'FETCH_USER', payload: response.data });
         }
-        console.log(error.config);
-    });
-
-    if (response) {
-        dispatch({ type: 'FETCH_USER', payload: response.data });
     }
+    else{dispatch({ type: 'FETCH_USER', payload: cache });}   
 }
 
 export const clearUser = () => (dispatch: Dispatch<TfetchUser>) => {
@@ -78,25 +90,15 @@ export const fetchAUserPosts = (username: string, page: number) => async (dispat
             per_page: 9
         },
     }).catch(function (error) {
-        if (error.response) {
-            if(error.response.status===401){
-                dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : "401"}})
-            }                      //request was made but server responded with status code different then 2xx
-            else if(error.response.status===404){
-                dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : "404"}})
-            }
-        } else if (error.request) {                 //request was made but no response received
-            dispatch({type: 'FETCH_USER_ERROR', payload: {err : true , errMessage : error.message}})
-        } else {                                    //error in request config
-            console.log('Error', error.message);
-        }
-        console.log(error.config);
+        
     });;
 
-    if(response)
+    if (response) {
         ((page === 1) ?
             dispatch({ type: 'FETCH_USER_POSTS_FIRST', payload: response.data }) :
             dispatch({ type: 'FETCH_USER_POSTS', payload: response.data }))
+    }
+
 
 }
 
