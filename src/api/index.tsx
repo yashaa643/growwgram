bd3930dc0a8be43feb8558d961638ce907bf5a1e
Yash Goyal <yashaa643@gmail.com>
2021-08-16 15:06:01 +0530

    hotfix : blur hash

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID RHfBZaVDqW4_C819eR078r2eCRIdjewiN9MxXktxWnM'
+        Authorization: 'Client-ID t_zYzW6qJZfVHwK860gWG_8KCC1zyCDH_oWoGF-0xlE'
       }
 })
