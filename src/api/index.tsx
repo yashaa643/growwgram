375a72cd64e502f7685cabd822697a4d43d8c64b
Yash Goyal <yashaa643@gmail.com>
2021-08-16 11:01:05 +0530

    fix : handling 403 error

--- src/api/index.tsx
+++ src/api/index.tsx
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
     baseURL: 'https://api.unsplash.com',
     headers: {
-        Authorization: 'Client-ID VNU24jfOdZjeWgQC_K4PGs2QTbNnPdbS1qpFclVLlmA'
+        Authorization: 'Client-ID RHfBZaVDqW4_C819eR078r2eCRIdjewiN9MxXktxWnM'
       }
 })
